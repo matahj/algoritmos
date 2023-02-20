b726875a1ff0f4319609ee0cc03e5fa92b9308fa
JorgeMataH <matahj@gmail.com>
2023-02-20 09:08:58 -0600

    bubble sort

---
+++ src/main/java/org/examples/t1_intro/Ej03_BubbleSort.java
@@ -0,0 +1,38 @@
+package org.examples.t1_intro;
+
+import java.util.Scanner;
+
+import static org.examples.matrices.OpsMatrices.generarArreglo;
+import static org.examples.matrices.OpsMatrices.imprimeArreglo;
+
+public class Ej03_BubbleSort {
+    public static void main(String[] args) {
+        int n;
+        int[] A;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Indique el valor de n: ");
+        n = scanner.nextInt();
+
+        A = generarArreglo(n);
+        imprimeArreglo(A,n);
+
+        bubbleSort(A,n);
+
+        imprimeArreglo(A,n);
+    }
+
+    private static void bubbleSort(int[] A, int n) {
+        int aux;
+
+        for(int i = n-1; i>0; i--){
+            for(int j=1; j < i; j++){
+                if(A[j-1] > A[j]){
+                    aux = A[j-1];
+                    A[j-1] = A[j];
+                    A[j] = aux;
+                }
+            }
+        }
+    }
+}
